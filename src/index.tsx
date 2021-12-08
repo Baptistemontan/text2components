6837f45cd2895815169ad7e04c1b7dc537eef7d8
Bamontan <baptiste.demontangon@gmail.com>
2021-12-08 11:48:20 +0100

    now can have recursive elements

--- src/index.tsx
+++ src/index.tsx
@@ -1,103 +1,113 @@
-import React, {
-  cloneElement,
-  ReactElement,
-  ReactNode,
-} from "react";
+import React from "react";
+import { cloneElement, ReactElement, ReactNode } from "react";
+
+type RegexResult = [string, string, RegexResult[]]; // before, tag, content
+
+const openTagRegex = /(<\s*?[^/>]+\s*?>)/g;
+const closeTagRegex = (str: string) => new RegExp(`(</\\s*?${str}\\s*?>)`, "g");
+const matchingOpenTagRegex = (str: string) =>
+  new RegExp(`<\\s*?${str}\\s*?>`, "g");
+
+function getPairs(str: string): RegexResult[] {
+  const [beforeSplit, tagSplit, ...afterSplitArray] = str.split(openTagRegex);
+  if (!tagSplit) {
+    return [[str, "", []]];
+  }
+  let after = afterSplitArray?.join("") || "";
+  let depth = 1;
+  let content = "";
+  const tag = tagSplit.slice(1, -1).trim();
+  while (depth > 0) {
+    const [before, _tag, ..._after] = after.split(closeTagRegex(tag));
+    if (!_tag) {
+      // error
+      return [
+        [beforeSplit + tagSplit, "", getPairs(afterSplitArray?.join("") || "")],
+      ];
+    }
+    depth -= 1;
+    depth += before.match(matchingOpenTagRegex(tag))?.length || 0;
+    after = _after?.join("") || "";
+    content += before;
+    if (depth) {
+      content += _tag;
+    }
+  }
+
+  return [[beforeSplit, tag, getPairs(content)], ...getPairs(after)];
+}
 
 export function valueInterpolation(
   text: string,
-  query: Record<string, string | number>
+  query: Record<string, string | number>,
 ): string {
-  if (!text || !query) return text || ''
+  if (!text || !query) return text || "";
 
   const escapeRegex = (str: string) =>
-    str.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')
-  
-  const prefix = '{{', suffix = '}}';
+    str.replace(/[-/\\^$*+?.()|[\]{}]/g, "\\$&");
 
-  const regexEnd = `\\s*${escapeRegex(suffix)}`
+  const prefix = "{{";
+  const suffix = "}}";
+
+  const regexEnd = `\\s*${escapeRegex(suffix)}`;
   return Object.keys(query).reduce((all, varKey) => {
     const regex = new RegExp(
       `${escapeRegex(prefix)}\\s*${varKey}${regexEnd}`,
-      'gm'
-    )
-    all = all.replace(regex, `${query[varKey]}`)
-    return all
-  }, text)
-}
-
-const tagRe = /<(\w+)>(.*?)<\/\1>|<(\w+)\/>/; // <tag>content</tag>
-const nlRe = /(?:\r\n|\r|\n)/g;
-
-function getElements(
-  parts: Array<string | undefined>,
-): Array<string | undefined>[] {
-  if (!parts.length) return [];
-
-  const [paired, children, unpaired, after] = parts.slice(0, 4);
-
-  return [[paired || unpaired || "", children || "", after]].concat(
-    getElements(parts.slice(4, parts.length)),
-  );
+      "gm",
+    );
+    // eslint-disable-next-line no-param-reassign
+    all = all.replace(regex, `${query[varKey]}`);
+    return all;
+  }, text);
 }
 
 function formatElements(
-  value: string,
-  elements: Record<string, ReactElement>
-): ReactNode[] | string {
-  const parts = value.replace(nlRe, "").split(tagRe);
-
-  if (parts.length === 1) return value;
-
-  const tree: Array<string | ReactElement> = [];
-
-  const before = parts.shift();
-  if (before) tree.push(before);
-
-  getElements(parts).forEach(([key, children, after], realIndex: number) => {
+  elements: Record<string, ReactElement>,
+  parts: RegexResult[],
+): (ReactNode | string)[] {
+  return parts.flatMap(([before, key, content], realIndex) => {
     const element = (key && elements[key]) || <></>;
-
-    tree.push(
+    return [
+      before,
       cloneElement(
         element,
-        { key: realIndex },
-
+        { key: realIndex + before + key },
         // format children for pair tags
         // unpaired tags might have children if it's a component passed as a variable
-        children ? formatElements(children, elements) : element.props.children,
+        content ? formatElements(elements, content) : element.props.children,
       ),
-    );
-
-    if (after) tree.push(after);
+    ];
   });
-
-  return tree;
 }
 
 export interface ComponentsInterpolationProps {
-    text: string,
-    components: Record<string, ReactElement>
+  text: string;
+  components: Record<string, ReactElement>;
 }
 
 export function ComponentsInterpolation({
-    text,
-    components,
-}:ComponentsInterpolationProps){
-    return <>{formatElements(text, components)}</>;
+  text,
+  components,
+}: ComponentsInterpolationProps) {
+  return <>{formatElements(components, getPairs(text))}</>;
 }
-
 
 export interface InterpolationProps {
-    text: string;
-    values?: Record<string, string | number>;
-    components?: Record<string, ReactElement>;
+  text: string;
+  values?: Record<string, string | number>;
+  components?: Record<string, ReactElement>;
 }
 
-export function Interpolation({ text, values, components }: InterpolationProps) {
+export function Interpolation({
+  text,
+  values,
+  components,
+}: InterpolationProps) {
+  const interpolatedValues = values ? valueInterpolation(text, values) : text;
 
-    const interpolatedValues = values ? valueInterpolation(text, values) : text;
+  const result = components
+    ? formatElements(components, getPairs(interpolatedValues))
+    : interpolatedValues;
 
-    const result = components ? formatElements(interpolatedValues, components) : interpolatedValues;
-
-    return <>{result}</>
+  return <>{result}</>;
 }
